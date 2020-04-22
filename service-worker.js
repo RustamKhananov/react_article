da34924a508bda134ad863dac0a67c5873a291a8
Rustam Khananov <antmine2017@gmail.com>
2020-04-22 16:10:53 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.e33179686e0a812d462427d7f94f9330.js"
 );
 
 self.addEventListener('message', (event) => {
